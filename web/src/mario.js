9dcbd7c23ea4138bce2b06af5db1ee6c1166b793
Hauke Schnau <hauke@schnau-lilienthal.de>
2019-11-25 22:49:59 +0100

    Improved Collision

--- web/src/mario.js
+++ web/src/mario.js
@@ -27,8 +27,7 @@ class Mario {
         this.ducked ? DUCKED_SIZE : 1.5
       );
       if (collision) {
-        const realPos = collision;
-        this.pos.x = realPos.x + 1 ;
+        this.pos.x = collision.x + 1;
       } else {
         this.pos = nextPos;
       }
@@ -42,8 +41,7 @@ class Mario {
         this.ducked ? DUCKED_SIZE : 1.5
       );
       if (collision) {
-        const realPos = collision;
-        this.pos.x = realPos.x - 1;
+        this.pos.x = collision.x - 1;
       } else {
         this.pos = nextPos;
       }
@@ -56,15 +54,17 @@ class Mario {
       this.ducked = true;
       this.pos = this.pos.add(0, 0.5);
     } else {
-      this.ducked = false
-      const nextPos = this.pos.sub(0, 0.5);
-      const collision = collide(
-        nextPos,
-        this.map,
-        this.ducked ? DUCKED_SIZE : 1.5
-      );
-      if (collision) {
-        this.ducked = true;
+      if (this.ducked) {
+        this.ducked = false;
+        const nextPos = this.pos.sub(0, 0.5);
+        const collision = collide(
+          nextPos,
+          this.map,
+          this.ducked ? DUCKED_SIZE : 1.5
+        );
+        if (collision) {
+          this.ducked = true;
+        }
       }
     }
   }
@@ -77,9 +77,14 @@ class Mario {
       this.ducked ? DUCKED_SIZE : 1.5
     );
     if (collision) {
-      const realPos = collision;
-      this.pos.y = realPos.y - (this.ducked ? DUCKED_SIZE : 1.5);
-      this.jumpBlocked = false;
+      const headCollision = collision.subV(this.pos).y < 0;
+
+      if (headCollision) {
+        this.pos.y = collision.y + 1;
+      } else {
+        this.pos.y = collision.y - (this.ducked ? DUCKED_SIZE : 1.5);
+        this.jumpBlocked = false;
+      }
       this.marioJumpForce = new Vector(0, 0);
     } else {
       this.jumpBlocked = true;
