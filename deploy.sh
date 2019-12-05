50e424efd3c0ebecc64f99f23e07835440ae3069
Hauke Schnau <hauke@schnau-lilienthal.de>
2019-12-05 14:29:47 +0100

    Deploy Script now copies Database too

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,8 @@ npm run build
 cd ../server
 rsync -avz --exclude node_modules -L . deploy@edu-net.schule:juicy_soup/server
 ssh deploy@edu-net.schule "cd juicy_soup/server; npm i"
+
+# Database
+username=$(awk '{print $1}' ./mongocreds)
+password=$(awk '{print $2}' ./mongocreds)
+mongodump --archive --db juicy_soup | mongorestore --archive --drop --db juicy_soup --host=edu-net.schule --username $username --password $password --authenticationDatabase admin
